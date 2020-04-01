c8d935b6bfcfbec1b49440150cc23b04336ba676
LuizHenriqueMaia <luizhenriquembh@gmail.com>
2020-04-01 20:35:24 -0300

    Implementing react

--- leadmanager/leadmanager/urls.py
+++ leadmanager/leadmanager/urls.py
@@ -3,6 +3,8 @@ from django.urls import path, include
 
 
 urlpatterns = [
+    # front-end has to render before the leads
+    path('', include('frontend.urls')),
     path('', include('main.urls')),
     path('admin/', admin.site.urls),
 ]
