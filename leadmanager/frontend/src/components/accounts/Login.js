fe5dceb03d5136b4d4263491908f3acc761148b6
LuizHenriqueMaia <luizhenriquembh@gmail.com>
2020-04-18 18:11:10 -0300

    Implementing auth state and private routes

---
+++ leadmanager/frontend/src/components/accounts/Login.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+
+
+export default class Login extends Component {
+    // component state, has nothing to do with redux
+    state = {
+        username: '',
+        password: '',
+    }
+
+    onSubmit = e => {
+        e.preventDefault()
+        console.log('submit')
+    }
+
+    onChange = e => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    render() {
+        const { username, password } = this.state
+        return (
+            <div className="col-md-6 m-auto">
+                <div className="card card-body mt-5">
+                    <h2 className="text-center">Login</h2>
+                    <form onSubmit={this.onSubmit}>
+                        <div className="form-group">
+                            <label>Username</label>
+                            <input type="text" className="form-control" name="username" onChange={this.onChange} value={username} />
+                        </div>
+                        <div className="form-group">
+                            <label>Password</label>
+                            <input type="password" className="form-control" name="password" onChange={this.onChange} value={password} />
+                        </div>
+                        <div className="form-group">
+                            <button type="submit" className="btn btn-primary">
+                                Register
+                            </button>
+                        </div>
+                        <p>
+                            Don't have a account?
+                            <Link to="/register">Register</Link>
+                        </p>
+                    </form>
+                </div>
+            </div>
+        )
+    }
+}
--- leadmanager/frontend/src/reducers/index.js
+++ leadmanager/frontend/src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux'
 import leads from './leads'
 import errors from './errors'
 import messages from './messages'
+import auth from './auth'
 
 
 export default combineReducers({
     // all the lines is obtain the same result with different syntax
     leads: leads,
     errors,
-    messages
+    messages,
+    auth
 })
